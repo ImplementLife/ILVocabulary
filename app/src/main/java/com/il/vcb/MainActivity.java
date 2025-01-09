67f6c3f30d4329170fd01b2068f361ec7bb911a6
ImplementLife <implementlifep@gmail.com>
2025-01-09 04:24:46 +0200

    init

---
+++ app/src/main/java/com/il/vcb/MainActivity.java
@@ -0,0 +1,13 @@
+package com.il.vcb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
