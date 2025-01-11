44ecd5d84ffc914d582211cbe2ed53c5ad1ba1c4
ImplementLife <implementlifep@gmail.com>
2025-01-11 23:13:36 +0200

    impl bottom nav p4

--- app/src/main/java/com/il/vcb/ui/activity/MainActivity.java
+++ app/src/main/java/com/il/vcb/ui/activity/MainActivity.java
@@ -11,6 +11,8 @@ import androidx.navigation.ui.NavigationUI;
 import com.il.vcb.R;
 import com.il.vcb.databinding.ActivityMainBinding;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
     public static MainActivity getInstance() {
         return instance;
@@ -23,16 +25,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //hide top bar with app name
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        instance = this;
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+        AppBarConfiguration appBarConfiguration =
+            new AppBarConfiguration.Builder(
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications
+            )
             .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
