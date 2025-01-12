9aca40d63ad0543294722c2f1c75c0a511a4cad8
ImplementLife <implementlifep@gmail.com>
2025-01-13 00:22:54 +0200

    change choose word pair sort dir

--- app/src/main/java/com/il/vcb/data/jpa/provide/WordDao.java
+++ app/src/main/java/com/il/vcb/data/jpa/provide/WordDao.java
@@ -20,6 +20,6 @@ public interface WordDao extends RestDao<Integer, Word> {
     @Query("SELECT COUNT(*) FROM il_word")
     int getCount();
 
-    @Query("SELECT * FROM il_word ORDER BY countCompleteRepeats DESC LIMIT 10")
+    @Query("SELECT * FROM il_word ORDER BY countCompleteRepeats ASC LIMIT 10")
     List<Word> getSomeByCountCompleteRepeatsDesc();
 }
