842db3c3ef7f2ebe72ec46043e338421b94a52dc
ImplementLife <implementlifep@gmail.com>
2025-01-24 21:44:27 +0200

    impl add word screen (p2)

--- app/src/main/java/com/il/lexicon/ui/fragment/AddWordFragment.java
+++ app/src/main/java/com/il/lexicon/ui/fragment/AddWordFragment.java
@@ -1,20 +1,10 @@
 package com.il.lexicon.ui.fragment;
 
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
 import com.il.lexicon.R;
 import com.il.lexicon.ui.custom.component.BaseFragment;
 
 public class AddWordFragment extends BaseFragment {
     public AddWordFragment() {
         super(R.layout.fragment_add_word);
-    }
-
-    public NavController navController;
-
-    @Override
-    protected void init() {
-//        NavHostFragment navHostFragment = (NavHostFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.cv_nav);
-//        navController = navHostFragment.getNavController();
     }
 }
